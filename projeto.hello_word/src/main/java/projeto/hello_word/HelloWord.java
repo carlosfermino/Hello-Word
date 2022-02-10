991db8274faa536b0a461fef01d9ebc20f47fc67
Carlos Fermino <brutosacf@gmail.com>
2022-02-10 09:34:32 -0300

    Alterado

--- projeto.hello_word/src/main/java/projeto/hello_word/HelloWord.java
+++ projeto.hello_word/src/main/java/projeto/hello_word/HelloWord.java
@@ -3,6 +3,7 @@ package projeto.hello_word;
 public class HelloWord {
 	public static void main(String[] args) {
 		System.out.println("Hello Word !!!!");
+		System.out.println("Projeto alterado");
 	}
 
 }
